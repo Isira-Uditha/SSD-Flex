aebcd8c4d9c270ad47083779ac2f3bc0a53bfc0a
Isira Uditha <isira@edhirya.com>
2022-11-02 19:51:57 +0530

    server initialized

---
+++ Flex-server/src/main/java/com/example/Flexserver/FlexServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flexserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexServerApplication.class, args);
+	}
+
+}
---
+++ Flex-server/src/test/java/com/example/Flexserver/FlexServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Flexserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
