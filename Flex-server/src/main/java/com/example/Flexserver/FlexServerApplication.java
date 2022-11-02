d0e8e95840bb16362d9c1ad4b738c46f498f4a4a
Isira Uditha <isira@edhirya.com>
2022-11-03 00:03:01 +0530

    security configurations added

--- Flex-server/src/main/java/com/example/Flexserver/FlexServerApplication.java
+++ Flex-server/src/main/java/com/example/Flexserver/FlexServerApplication.java
@@ -2,8 +2,9 @@ package com.example.Flexserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class FlexServerApplication {
 
 	public static void main(String[] args) {
