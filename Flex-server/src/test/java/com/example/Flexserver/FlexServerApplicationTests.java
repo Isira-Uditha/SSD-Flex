769e1ffdf39a0b69dda680d4aa39ed7d33f3d883
Isira Uditha <isira@edhirya.com>
2022-11-03 23:27:19 +0530

    testcase added

--- Flex-server/src/test/java/com/example/Flexserver/FlexServerApplicationTests.java
+++ Flex-server/src/test/java/com/example/Flexserver/FlexServerApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.Flexserver;
 
+import com.example.Flexserver.domain.model.User;
+import com.example.Flexserver.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class FlexServerApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	public void testUserCreate() {
+		User user = new User();
+		user.setUsername("isira.uditha");
+		user.setName("Isira Uditha");
+		user.setRole("manager");
+		user.setPassword("123");
+		assertThat(userRepository.createUser(user));
+
+
+
 	}
 
 }
